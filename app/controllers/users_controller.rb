f48ba68492bb3151b499920767e931d7e0c6897d
Sean Griffin <sgriffin@griffinassoc.com>
2012-03-05 16:41:29 -0700

    Fucking vim man

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to user_path, notice: 'Account was successfully updated.' }
+        format.html { redirect_to user_path, notice: 'Account details successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }

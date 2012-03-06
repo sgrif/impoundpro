69a29c3f827237c86cd0c4a0dbe7df131f0c69f8
Sean Griffin <sgriffin@griffinassoc.com>
2012-03-06 14:54:56 -0700

    More key value format changes

--- db/migrate/20120303002431_add_charges_to_cars.rb
+++ db/migrate/20120303002431_add_charges_to_cars.rb
@@ -1,8 +1,8 @@
 class AddChargesToCars < ActiveRecord::Migration
   def change
-    add_column :cars, :charge_hook_up, :decimal, precision: 8, scale: 2
+    add_column :cars, :charge_hook_up, :decimal, :precision => 8, :scale => 2
 
-    add_column :cars, :charge_other, :decimal, precision: 8, scale: 2
+    add_column :cars, :charge_other, :decimal, :precision => 8, :scale => 2
     
     rename_column :cars, :charge_towing, :charge_mileage
 

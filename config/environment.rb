1d70c4933f3b44b8bf975dad92f559478ad2bd34
Sean Griffin <sgriffin@griffinassoc.com>
2012-04-02 15:29:44 -0600

    Forcing dev environment

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,6 @@ Tyler::Application.initialize!
 Time::DATE_FORMATS[:month_and_day] = "%B %d"
 Time::DATE_FORMATS[:short_year] = "%y"
 Time::DATE_FORMATS[:short_date] = "%m/%d/%Y"
+
+# TODO Remove this for deployment
+ENV['RAILS_ENV'] ||= 'production'

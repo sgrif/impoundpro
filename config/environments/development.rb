9ff85af8ef820f5527be92657ad9ddd830a58426
Sean Griffin <sean.griffin@boomtime.com>
2012-05-07 16:34:41 -0600

    Temp mail fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Tyler::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  
+
   config.action_mailer.default_url_options = { :host => "0.0.0.0:3000" }
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,13 @@ Tyler::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = {:host => "http://www.impoundpro.com/"}
-  ActionMailer::Base.default :from => "info@impoundpro.com"
+  config.action_mailer.default_url_options = { :host => "0.0.0.0:3000" }
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'ferret4prez@gmail.com',
+    :password => 'Me345678',
+    :authentication => 'login'
+  }
+  ActionMailer::Base.default :from => 'ferret4prez@gmail.com'
 end

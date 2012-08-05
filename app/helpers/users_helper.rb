7ddf9478e722ca1c864c0e94143be0cda44581e7
Sean Griffin <sean.griffin@boomtime.com>
2012-08-05 17:58:42 -0600

    Minor sidebar changes
    Moved avatar_url into UsersHelper. Changed profile in sidebar to
    settings.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=50&d=mm&r=g"
+  end
 end

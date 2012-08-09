eddd3ae2d9bae114c8bb47630c5a4333da1b04e6
Sean Griffin <sean.griffin@boomtime.com>
2012-08-09 07:08:03 -0600

    Added reports page. Closes #8

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ ImpoundPro::Application.routes.draw do
 
     match 'cars/unclaimed_vehicles_report' => 'cars#unclaimed_vehicles_report'
 
-    get 'reports' => 'secure#dashboard'
+    get 'reports' => 'secure#reports'
 
     resources :cars do
       member do

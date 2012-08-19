598dcb80677be5c541587e2c67187d98a78099ec
Sean Griffin <sean.griffin@boomtime.com>
2012-08-18 19:53:02 -0600

    Fuck whoever invented time zones they are assholes.
    1.9.3p194 :001 > Date.yesterday
    => Sat, 18 Aug 2012
    1.9.3p194 :002 > Date.today
    => Sat, 18 Aug 2012
    
    Wat?

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -25,10 +25,8 @@ module CarsHelper
 
   def car_step_tag(name, path, *args)
     html_options = args.extract_options!
-    if args.first
-      content_tag :span, "<i class='icon-check'></i> #{name}".html_safe, class: "btn active"
-    elsif args.second
-      content_tag :span, "<i class='icon-check-empty'></i> #{name}".html_safe, class: "btn disabled"
+    if args.first or args.second
+      content_tag :span, "<i class='icon-check#{"-empty" if args.second}'></i> #{name}".html_safe, class: "btn disabled"
     else
       html_options[:class] = "btn"
       link_to "<i class='icon-check-empty'></i> #{name}".html_safe, path, html_options
